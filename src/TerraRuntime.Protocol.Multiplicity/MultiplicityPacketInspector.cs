dd8efa973e8d9a516f806a7e9355fcab82134302
Xekep <Xekep@users.noreply.github.com>
2026-08-27 15:20:38 +0300

    protocol: wrap Multiplicity packet views

---
+++ src/TerraRuntime.Protocol.Multiplicity/MultiplicityPacketInspector.cs
@@ -0,0 +1,23 @@
+using Multiplicity.Packets.Views;
+
+namespace TerraRuntime.Protocol.Multiplicity;
+
+public static class MultiplicityPacketInspector
+{
+    public static bool TryReadHeader(ReadOnlySpan<byte> buffer, out PacketHeaderInfo header)
+    {
+        if (!PacketViewParser.TryReadHeader(buffer, out PacketHeader parsed))
+        {
+            header = default;
+            return false;
+        }
+
+        header = new PacketHeaderInfo(parsed.PacketLength, parsed.RawPacketId, parsed.PayloadLength);
+        return true;
+    }
+
+    public static bool IsCompletePacket(ReadOnlySpan<byte> buffer)
+    {
+        return PacketViewParser.TryParseExact(buffer, out _);
+    }
+}
