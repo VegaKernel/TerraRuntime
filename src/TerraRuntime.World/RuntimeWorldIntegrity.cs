ee36b37247b35aa87f2fed4912d12ddf0a20e701
Xekep <Xekep@users.noreply.github.com>
2026-08-28 20:29:05 +0300

    perf: streamline runtime snapshot integrity hashing

--- src/TerraRuntime.World/RuntimeWorldIntegrity.cs
+++ src/TerraRuntime.World/RuntimeWorldIntegrity.cs
@@ -1,5 +1,7 @@
 using System.Buffers.Binary;
 using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace TerraRuntime.World;
 
@@ -16,25 +18,28 @@ internal static class RuntimeWorldIntegrity
     private const ulong Prime4 = 9650029242287828579UL;
     private const ulong Prime5 = 2870177450012600261UL;
 
+    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
     public static ulong Hash64(ReadOnlySpan<byte> data)
     {
+        int length = data.Length;
         int offset = 0;
         ulong hash;
+        ref byte source = ref MemoryMarshal.GetReference(data);
 
-        if (data.Length >= 32)
+        if (length >= 32)
         {
             ulong v1 = unchecked(Prime1 + Prime2);
             ulong v2 = Prime2;
             ulong v3 = 0;
             ulong v4 = unchecked(0UL - Prime1);
-            int limit = data.Length - 32;
+            int limit = length - 32;
 
             do
             {
-                v1 = Round(v1, BinaryPrimitives.ReadUInt64LittleEndian(data[offset..]));
-                v2 = Round(v2, BinaryPrimitives.ReadUInt64LittleEndian(data[(offset + 8)..]));
-                v3 = Round(v3, BinaryPrimitives.ReadUInt64LittleEndian(data[(offset + 16)..]));
-                v4 = Round(v4, BinaryPrimitives.ReadUInt64LittleEndian(data[(offset + 24)..]));
+                v1 = Round(v1, ReadUInt64LittleEndian(ref source, offset));
+                v2 = Round(v2, ReadUInt64LittleEndian(ref source, offset + 8));
+                v3 = Round(v3, ReadUInt64LittleEndian(ref source, offset + 16));
+                v4 = Round(v4, ReadUInt64LittleEndian(ref source, offset + 24));
                 offset += 32;
             }
             while (offset <= limit);
@@ -53,26 +58,26 @@ internal static class RuntimeWorldIntegrity
             hash = Prime5;
         }
 
-        hash = unchecked(hash + (ulong)data.Length);
+        hash = unchecked(hash + (ulong)length);
 
-        while (offset <= data.Length - 8)
+        while (offset <= length - 8)
         {
-            ulong lane = Round(0, BinaryPrimitives.ReadUInt64LittleEndian(data[offset..]));
+            ulong lane = Round(0, ReadUInt64LittleEndian(ref source, offset));
             hash ^= lane;
             hash = unchecked((BitOperations.RotateLeft(hash, 27) * Prime1) + Prime4);
             offset += 8;
         }
 
-        if (offset <= data.Length - 4)
+        if (offset <= length - 4)
         {
-            hash ^= unchecked((ulong)BinaryPrimitives.ReadUInt32LittleEndian(data[offset..]) * Prime1);
+            hash ^= unchecked((ulong)ReadUInt32LittleEndian(ref source, offset) * Prime1);
             hash = unchecked((BitOperations.RotateLeft(hash, 23) * Prime2) + Prime3);
             offset += 4;
         }
 
-        while (offset < data.Length)
+        while (offset < length)
         {
-            hash ^= unchecked(data[offset] * Prime5);
+            hash ^= unchecked(Unsafe.Add(ref source, offset) * Prime5);
             hash = unchecked(BitOperations.RotateLeft(hash, 11) * Prime1);
             offset++;
         }
@@ -85,6 +90,21 @@ internal static class RuntimeWorldIntegrity
         return hash;
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static ulong ReadUInt64LittleEndian(ref byte source, int offset)
+    {
+        ulong value = Unsafe.ReadUnaligned<ulong>(ref Unsafe.Add(ref source, offset));
+        return BitConverter.IsLittleEndian ? value : BinaryPrimitives.ReverseEndianness(value);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint ReadUInt32LittleEndian(ref byte source, int offset)
+    {
+        uint value = Unsafe.ReadUnaligned<uint>(ref Unsafe.Add(ref source, offset));
+        return BitConverter.IsLittleEndian ? value : BinaryPrimitives.ReverseEndianness(value);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static ulong Round(ulong accumulator, ulong input)
     {
         accumulator = unchecked(accumulator + (input * Prime2));
@@ -92,6 +112,7 @@ internal static class RuntimeWorldIntegrity
         return unchecked(accumulator * Prime1);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static ulong MergeRound(ulong accumulator, ulong value)
     {
         accumulator ^= Round(0, value);
