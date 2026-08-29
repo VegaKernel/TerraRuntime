b7fd5b3b969692379c8309a58a663ad96c78488d
Xekep <Xekep@users.noreply.github.com>
2026-08-29 16:37:47 +0300

    gameplay: attach NPC actor operations to host runtime

--- src/TerraRuntime/TerraRuntimeHostRuntime.cs
+++ src/TerraRuntime/TerraRuntimeHostRuntime.cs
@@ -8,14 +8,17 @@ internal sealed class TerraRuntimeHostRuntime : ITerraRuntimeHostRuntime
     public TerraRuntimeHostRuntime(
         TerraRuntimeHostRuntimeInfo info,
         IInterestManagementControl interestManagement,
-        IPlayerStateSnapshotReader playerStates)
+        IPlayerStateSnapshotReader playerStates,
+        INpcActorOperations npcActors)
     {
         Info = info ?? throw new ArgumentNullException(nameof(info));
         InterestManagement = interestManagement ?? throw new ArgumentNullException(nameof(interestManagement));
         PlayerStates = playerStates ?? throw new ArgumentNullException(nameof(playerStates));
+        NpcActors = npcActors ?? throw new ArgumentNullException(nameof(npcActors));
     }
 
     public TerraRuntimeHostRuntimeInfo Info { get; }
     public IInterestManagementControl InterestManagement { get; }
     public IPlayerStateSnapshotReader PlayerStates { get; }
+    public INpcActorOperations NpcActors { get; }
 }
