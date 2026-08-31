341e947cfa239bc5852fcf04dcc94a494e42390f
Xekep <Xekep@users.noreply.github.com>
2026-08-31 21:59:58 +0300

    Fix King Slime instanced drop protocol namespace

--- src/TerraRuntime/RuntimeKingSlimeDifficultyLootDeliverySink.cs
+++ src/TerraRuntime/RuntimeKingSlimeDifficultyLootDeliverySink.cs
@@ -1,6 +1,7 @@
 using TerraRuntime.Contracts.Gameplay;
 using TerraRuntime.Contracts.Runtime;
 using TerraRuntime.Core;
+using TerraRuntime.Protocol;
 using TerraRuntime.Protocol.Multiplicity;
 
 namespace TerraRuntime;
