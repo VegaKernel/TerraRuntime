2b036982e9fcd0b37d6e084de66cafd64aef7068
Xekep <Xekep@users.noreply.github.com>
2026-09-20 05:48:43 +0300

    Cover trapdoor packet ingress

--- tests/TerraRuntime.Tests/ProductionTileSinkCompositionTests.cs
+++ tests/TerraRuntime.Tests/ProductionTileSinkCompositionTests.cs
@@ -150,6 +150,29 @@ public sealed class ProductionTileSinkCompositionTests
         Assert.Equal(PlayerDoorToggleFrameStopReason.None, projectileSink.PlayerDoorToggleStopReason);
     }
 
+    [Theory]
+    [InlineData(TerrariaDoorToggleAction.OpenTrapdoor)]
+    [InlineData(TerrariaDoorToggleAction.CloseTrapdoor)]
+    public void Production_chest_outer_sink_routes_packet19_trapdoor_toggle_through_projectile_tile_composition(
+        TerrariaDoorToggleAction action)
+    {
+        GameCommandSourceId source = GameCommandSourceId.FromConnection(9022 + (int)action);
+        using PlayerBootstrapFrameSink bootstrap = CreatePlayingBootstrap(source);
+        var commands = new RecordingCommandIngress();
+        var gameplayIngress = new RuntimeProjectileNetworkIngress(commands);
+        var projectileSink = new ProjectileLifecycleFrameSink(source, bootstrap, new PassthroughSink(), gameplayIngress);
+        var chestSink = new ChestInteractionFrameSink(source, bootstrap, projectileSink, new AcceptingChestIngress());
+        var packet = new TerrariaDoorToggleState((byte)action, TileX: 40, TileY: 50, DirectionX: 1);
+
+        Assert.Equal(TerrariaFrameSinkResult.Continue, chestSink.OnFrame(Packet19(in packet)));
+
+        ClientTrapdoorToggleRuntimeCommand command = Assert.IsType<ClientTrapdoorToggleRuntimeCommand>(commands.Command);
+        Assert.Equal(source, commands.Source);
+        Assert.Equal(bootstrap.AssignedPlayerHandle, command.Connection.Player);
+        Assert.Equal(packet, command.State);
+        Assert.Equal(PlayerDoorToggleFrameStopReason.None, projectileSink.PlayerDoorToggleStopReason);
+    }
+
 
     [Theory]
     [InlineData(3509, false)] // Copper Pickaxe
